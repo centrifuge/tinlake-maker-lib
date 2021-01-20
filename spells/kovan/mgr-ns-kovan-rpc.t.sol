08acb83230a65393164a445b5ac82b9f5c1f18c0
ilinzweilin <alina@centrifuge.io>
2021-01-20 19:43:35 +0100

    add sep spell folder

--- spells/kovan/mgr-ns-kovan-rpc.t.sol
+++ spells/kovan/mgr-ns-kovan-rpc.t.sol
@@ -2,8 +2,8 @@ pragma solidity >=0.5.12;
 
 import "ds-test/test.sol";
 import "../mgr.sol";
-import {DssSpell} from "../spell.sol";
-import "lib/dss-interfaces/src/Interfaces.sol";
+import {DssSpell} from "./ns-spell-kovan.sol";
+import "../../lib/dss-interfaces/src/Interfaces.sol";
 import {DSValue} from "ds-value/value.sol";
 import {EpochCoordinator} from "tinlake/lender/coordinator.sol";
 
@@ -44,7 +44,6 @@ contract TinlakeManagerTest is DSTest {
     function mul(uint x, uint y) internal pure returns (uint z) {
         require(y == 0 || (z = x * y) / y == x);
     }
-
     // MCD
     VatAbstract vat;
     CatAbstract cat;
@@ -52,25 +51,27 @@ contract TinlakeManagerTest is DSTest {
     SpotAbstract spotter;
     DaiAbstract dai;
     JugAbstract jug;
+    //  https://changelog.makerdao.com/releases/kovan/1.2.2/contracts.json
     ChainlogAbstract constant CHANGELOG = ChainlogAbstract(0xdA0Ab1e0017DEbCd72Be8599041a2aa3bA7e740F);
     DSChiefAbstract chief;
     DSTokenAbstract gov;
     address pause_proxy;
 
-    //  -- testing --
+    // -- testing --                 
     Hevm constant hevm = Hevm(0x7109709ECfa91a80626fF3989D68f67F5b1DD12D);
     DssSpell spell;
     TinlakeManager dropMgr;
     DSValue dropPip;
 
     // Tinlake
-    address constant reserve = 0x30FDE788c346aBDdb564110293B20A13cF1464B6;
-    GemLike constant drop = GemLike(0xE4C72b4dE5b0F9ACcEA880Ad0b1F944F85A9dAA0);
-    Root constant root = Root(0x53b2d22d07E069a3b132BfeaaD275b10273d381E);
-    MemberList constant memberlist = MemberList(0x5B5CFD6E45F1407ABCb4BFD9947aBea1EA6649dA);
-    EpochCoordinator constant coordinator = EpochCoordinator(0xFE860d06fF2a3A485922A6a029DFc1CD8A335288);
-    address constant assessor_ = 0xdA0bA5Dd06C8BaeC53Fa8ae25Ad4f19088D6375b;
-    
+    GemLike constant drop = GemLike(0x352Fee834a14800739DC72B219572d18618D9846);
+    Root constant root = Root(0x25dF507570c8285E9c8E7FFabC87db7836850dCd);
+    address constant MCD_NS2DRP_MGR_A  = 0x65242F75e6cCBF973b15d483dD5F555d13955A1e; // NSDROP MGR
+    MemberList constant memberlist = MemberList(0xD927F069faf59eD83A1072624Eeb794235bBA652);
+    EpochCoordinator constant coordinator = EpochCoordinator(0xB51D3cbaa5CCeEf896B96091E69be48bCbDE8367);
+    address constant seniorOperator_ = 0x6B902D49580320779262505e346E3f9B986e99e8;
+    address constant seniorTranche_ = 0xDF0c780Ae58cD067ce10E0D7cdB49e92EEe716d9;
+    address constant assessor_ = 0x49527a20904aF41d1cbFc0ba77576B9FBd8ec9E5;
 
     function setUp() public {
         vat = VatAbstract(CHANGELOG.getAddress("MCD_VAT"));
@@ -83,21 +84,12 @@ contract TinlakeManagerTest is DSTest {
         gov = DSTokenAbstract(CHANGELOG.getAddress("MCD_GOV"));
         pause_proxy = CHANGELOG.getAddress("MCD_PAUSE_PROXY");
 
-
         // deploy unmodified pip
         dropPip = new DSValue();
         dropPip.poke(bytes32(uint(1 ether)));
 
-        // deploy dropMgr
-        dropMgr = new TinlakeManager(address(vat),
-                                     CHANGELOG.getAddress("MCD_DAI"),
-                                     CHANGELOG.getAddress("MCD_JOIN_DAI"),
-                                     address(vow),
-                                     0xE4C72b4dE5b0F9ACcEA880Ad0b1F944F85A9dAA0, // DROP token
-                                     0x230f2E19D6c2Dc0c441c2150D4dD9d67B563A60C, // senior operator
-                                     address(this),
-                                     0xfB30B47c47E2fAB74ca5b0c1561C2909b280c4E5, // senior tranche
-                                     ilk);
+        dropMgr = TinlakeManager(MCD_NS2DRP_MGR_A);
+
         // cast spell
         spell = new DssSpell();
         vote();
@@ -108,10 +100,16 @@ contract TinlakeManagerTest is DSTest {
 
         // welcome to hevm KYC
         hevm.store(address(root), keccak256(abi.encode(address(this), uint(0))), bytes32(uint(1)));
+
         root.relyContract(address(memberlist), address(this));
         memberlist.updateMember(address(this), uint(-1));
-        memberlist.updateMember(address(dropMgr), uint(-1));
 
+        // set this contract as owner of dropMgr // override slot 1
+        // check what's inside slot 1 with: bytes32 slot = hevm.load(address(dropMgr), bytes32(uint(1)));
+        hevm.store(address(dropMgr), bytes32(uint(1)), bytes32(0x0000000000000000000101013bE95e4159a131E56A84657c4ad4D43eC7Cd865d));
+        // ste this contract as ward on the mgr
+        hevm.store(address(dropMgr), keccak256(abi.encode(address(this), uint(0))), bytes32(uint(1)));
+        assertEq(dropMgr.owner(), address(this));
         // give this address 1500 dai and 1000 drop
         hevm.store(address(dai), keccak256(abi.encode(address(this), uint(2))), bytes32(uint(1500 ether)));
         hevm.store(address(drop), keccak256(abi.encode(address(this), uint(8))), bytes32(uint(1000 ether)));
@@ -130,6 +128,7 @@ contract TinlakeManagerTest is DSTest {
     function testJoinAndDraw() public {
         assertEq(dai.balanceOf(address(this)), 1500 ether);
         assertEq(drop.balanceOf(address(this)), 1000 ether);
+
         dropMgr.join(400 ether);
         dropMgr.draw(200 ether);
         assertEq(dai.balanceOf(address(this)), 1700 ether);
@@ -245,7 +244,7 @@ contract TinlakeManagerTest is DSTest {
     }
 
     function cdptab() public view returns (uint) {
-        //  Calculate DAI cdp debt
+        // Calculate DAI cdp debt
         (, uint art) = vat.urns(ilk, address(dropMgr));
         (, uint rate, , ,) = vat.ilks(ilk);
         return art * rate;
